9b93cc4f1f6e139ec542030b6300e6dcb345e92d
Abject <maxime.radomski@gmail.com>
2025-10-20 16:30:14 +0200

    Version 1.0.0 no AVIF

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"80f4a0f251b89b010a4760c7a055a4531c52c6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "222517417"
+    serviceWorkerVersion: "4012321363"
   }
 });
